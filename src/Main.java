b1cbc6455e1c7cb500f0a7cdf1d16c0da2a12713
concooking <kovardashov@gmail.com>
2024-11-17 11:36:20 +0300

    Проверьте ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Задача 1
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastNAme = "Ivanov";
+        String fullNAme = lastNAme + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О сотрудника " + fullNAme);
+        // Задача 2
+        System.out.println("Задача 2");
+        String fullNameCase = "Ivanov Ivan Ivanovich";
+        String f1 = fullNameCase.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + f1);
+        // Задача 3
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        String fullNameTrue = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameTrue);
     }
 }
+
+
+
+
+
